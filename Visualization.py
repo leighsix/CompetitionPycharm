40f1f2471374b6618abd42bb2e29e51bbdd4521b
leighsix <leighsix@naver.com>
2019-01-10 04:02:38 +0800

    집에서 하는 첫번째 작업... 시각화 작업 중... 등고선 작업중

--- Visualization.py
+++ Visualization.py
@@ -11,18 +11,49 @@ class Visualization:
     def __init__(self):
         self.select_db = SelectDB.SelectDB()
 
-    def plot_3D_for_average_state(self, table):
+    def plot_3D_for_average_state(self, table, step):
         df = self.select_db.select_data_from_DB(table)
-        df2 = df[df.Steps == 30]
+        df2 = df[df.Steps == step]
         sns.set_style("whitegrid")
         ax = plt.axes(projection='3d')
         ax.plot_trisurf(df2['beta'], df2['gamma'], (df2['LAYER_A_MEAN']+df2['LAYER_B_MEAN']),
-                         cmap='RdBu', edgecolor='none')
+                        cmap='RdBu', edgecolor='none')
         ax.set_xlabel('beta')
         ax.set_ylabel('gamma')
         ax.set_zlabel('Average States')
         ax.set_title('beta-gamma-States')
         ax.view_init(45, 45)
+
+    def plot_3D_to_2D_contour_for_average_state(self, table, step):
+        df = self.select_db.select_data_from_DB(table)
+        df2 = df[df.Steps == step]
+        sns.set_style("whitegrid")
+        result_beta = sorted(np.array(df2['beta']))
+        result_gamma = sorted(np.array(df2['gamma']))
+        result_state = np.array(df2['LAYER_A_MEAN'] + df2['LAYER_B_MEAN']).reshape(41, 41)
+
+        X, Y = np.meshgrid(result_beta, result_gamma)
+        Z = z_function(result, c)
+        plt.contourf(X, Y, Z, 50, cmap='RdBu')
+        plt.xlabel('beta')
+        plt.ylabel('gamma')
+        plt.colorbar(label='Average states')
+
+    @staticmethod
+    def z_function(result, a):
+        final_data = pd.read_pickle(result)
+        z = np.array(final_data[str(a)]).reshape(41, 41)
+        Z = np.zeros((1681, 1681))
+        for i in range(0, 41):
+            for j in range(0, 41):
+                for k in range(0, 41):
+                    for l in range(0, 41):
+                        Z[(i * 41) + k][(j * 41) + l] = z[i][j]
+        return Z
+
+
+
+
 
     def plot_2D_gamma_for_average_state(self, table, beta_min, beta_max):
         df = self.select_db.select_data_from_DB(table)
@@ -35,7 +66,6 @@ class Visualization:
         plt.ylim(-1.3, 1.3)
         plt.xlabel('gamma')
         plt.ylabel('Average States')
-
 
     def plot_2D_beta_for_average_state(self, table, gamma_min, gamma_max):
         df = self.select_db.select_data_from_DB(table)
@@ -50,18 +80,8 @@ class Visualization:
         plt.ylabel('Average States')
 
 
-
     def prob_beta_plot_3D(result, number_ganma, t, initial, gap, a, b, c, d, e):
         df = self.select_db.select_data_from_DB(table)
-
-
-
-
-
-
-
-
-        flow_probbeta_data = pd.read_pickle(result)
         ganma_data = []
         probbeta = []
         times = np.linspace(0, t - 1, t)
@@ -81,8 +101,8 @@ class Visualization:
         ax.view_init(d, e)
 
 
-# ex__  prob_beta_plot_3D('flow_prob_beta5.0_data.pickle', 51, 20, 10, 101, 'time', 'ganma', 'prob_beta', 45, 45)
 
+# ex__  prob_beta_plot_3D('flow_prob_beta5.0_data.pickle', 51, 20, 10, 101, 'time', 'ganma', 'prob_beta', 45, 45)
 def total_flow_prob_beta_chart(filename):
     plt.figure()
     sns.set()
@@ -134,27 +154,6 @@ def ganma_scale_for_chart(filename, y_axis, a, b):  # 0 < a, b < 3
         pic = final_table[ganma_scale[i]]
         plt.plot(pic, linewidth=0.3)
 
-    def z_function(result, a):
-        final_data = pd.read_pickle(result)
-        z = np.array(final_data[str(a)]).reshape(41, 41)
-        Z = np.zeros((1681, 1681))
-        for i in range(0, 41):
-            for j in range(0, 41):
-                for k in range(0, 41):
-                    for l in range(0, 41):
-                        Z[(i * 41) + k][(j * 41) + l] = z[i][j]
-        return Z
 
 
-    def plot_3D_to_2D_contour(result, a, b, c, d, e, f):
-        sns.set_style("whitegrid")
-        final_data = pd.read_pickle(result)
-        result_a = sorted(np.array(final_data[str(a)]))
-        result_b = sorted(np.array(final_data[str(b)]))
-        result_c = np.array(final_data[str(c)]).reshape(41, 41)
-        X, Y = np.meshgrid(result_a, result_b)
-        Z = z_function(result, c)
-        plt.contourf(X, Y, Z, 50, cmap='RdBu')
-        plt.xlabel(str(d))
-        plt.ylabel(str(e))
-        plt.colorbar(label=str(f))
+
