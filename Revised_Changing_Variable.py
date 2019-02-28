f80cbf3eeb94204da10f912e674f6f5986e69bf5
leighsix <33159157+leighsix@users.noreply.github.com>
2019-02-28 15:13:34 +0800

    no message

--- Revised_Changing_Variable.py
+++ Revised_Changing_Variable.py
@@ -1,5 +1,5 @@
 import numpy as np
-import Setting_Simulation_Value
+import Setting_Revised_Value
 import MakingPandas
 import RevisedRepeatDynamics
 import sqlalchemy
@@ -11,11 +11,11 @@ class Revised_Changing_Variable:
         self.mp = MakingPandas.MakingPandas()
         self.revised_repeat_dynamics = RevisedRepeatDynamics.RevisedRepeatDynamics(setting)
 
-    def calculate_and_input_database(self, setting, beta):
-        self.revised_repeat_dynamics.repeat_dynamics(setting, beta)
-        panda_db = self.mp.making_dataframe_per_step(setting, self.revised_repeat_dynamics.Num_Data)
+    def calculate_and_input_database(self, setting_variable_tuple):
+        self.revised_repeat_dynamics.repeat_dynamics(setting_variable_tuple[0], setting_variable_tuple[1])
+        panda_db = self.mp.making_dataframe_per_step(setting_variable_tuple[0], self.revised_repeat_dynamics.Num_Data)
         print(panda_db.loc[0])
-        panda_db['beta'] = beta
+        panda_db['beta'] = setting_variable_tuple[1]
         engine = sqlalchemy.create_engine('mysql+pymysql://root:2853@localhost:3306/renew_competition')
         panda_db.to_sql(name='revised_layer_state', con=engine, index=False, if_exists='append')
         print(panda_db.loc[0])  # 프로그램 잘 실행되고 있는지 확인을 위해서 프린트 실시
@@ -26,15 +26,15 @@ class Revised_Changing_Variable:
         workers = setting.workers
         setting_variable_list = []
         for i in setting.D:
-            setting_variable_list.append(i)
+            setting_variable_list.append((setting, i))
         with Pool(workers) as p:
             p.map(self.calculate_and_input_database, setting_variable_list)
 
 if __name__ == "__main__":
     print("Changing_Variable")
-    setting = Setting_Simulation_Value.Setting_Simulation_Value()
-    changing_variable = Changing_Variable(setting)
-    changing_variable.paralleled_work(setting)
+    setting = Setting_Revised_Value.Setting_Revised_Value()
+    revised_changing_variable = Revised_Changing_Variable(setting)
+    revised_changing_variable.paralleled_work(setting)
     print("Operating end")
 
 
