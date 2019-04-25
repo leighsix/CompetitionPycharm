e6f4f77fea45fc2a0ebf56154c17c614d30d6daa
leighsix <33159157+leighsix@users.noreply.github.com>
2019-04-25 21:39:56 +0800

    커밋

--- AnalysisDB.py
+++ AnalysisDB.py
@@ -15,24 +15,7 @@ class AnalysisDB:
     def __init__(self):
         self.select_db = SelectDB.SelectDB()
 
-    def making_property_array_for_new(self, setting, step):
-        property_array = np.zeros(5)
-        df_original = self.select_db.select_data_from_DB(setting)
-        df_step = df_original[df_original.Steps == step]
-        model = df_step['MODEL']
-        model = sorted(model.unique())
-        for m in model:
-            df_model = df_step[df_step.MODEL == m]
-            total_AS = self.calculate_total_AS_new(df_model)
-            pcr, ncr = self.calculate_consensus_number_new(df_model)
-            initial_value = np.array([m, total_AS, pcr, ncr, pcr+ncr])
-            property_array = np.vstack([property_array, initial_value])
-        property_data = property_array[1:]
-        columns = ['Model', 'AS total', 'PCR', 'NCR', 'CR']
-        df = pd.DataFrame(property_data, columns=columns)
-        return df
-
-    def making_property_array(self, setting, step):
+    def making_property_array(self, setting, step, a):
         property_array = np.zeros(11)
         df_original = self.select_db.select_data_from_DB(setting)
         df_step = df_original[df_original.Steps == step]
@@ -58,7 +41,7 @@ class AnalysisDB:
                             df_be = df_ae[df_ae.B_internal_edges == be]
                             total_AS = self.calculate_total_AS(setting, df_be)
                             total_fraction_ab = self.calculate_fraction_ab_total(df_be)
-                            pcr, ncr = self.calculate_consensus_number_old2(setting, df_be)
+                            pcr, ncr = self.calculate_consensus_number(setting, df_be, a)
                             initial_value = np.array([m, an, bn, ae, be, step,
                                                       total_AS, total_fraction_ab, pcr,
                                                       ncr, pcr+ncr])
@@ -69,61 +52,20 @@ class AnalysisDB:
         df = pd.DataFrame(property_data, columns=columns)
         return df
 
-
-    def calculate_total_AS_new(self, df):
-        total_AS = sum(df['AS']) / len(df)
-        return total_AS
-
-    def calculate_consensus_number_new(self, df):
-        pos_con = 0
-        neg_con = 0
-        signed_CI = df['AS']
-        for i in range(len(df)):
-            if signed_CI.iloc[i] > 0.95:
-                pos_con += 1
-            elif signed_CI.iloc[i] < -0.95:
-                neg_con += 1
-        return pos_con/len(df), neg_con/len(df)
-
     def calculate_total_AS(self, setting, df):
         AS = (df['LAYER_A_MEAN'] / setting.MAX + df['LAYER_B_MEAN']) / 2
         total_AS = sum(AS) / len(AS)
         return total_AS
 
-    def calculate_consensus_number(self, setting, df):
-        pos_con = 0
-        neg_con = 0
-        consensus = df['CONSENSUS']
-        signed_CI = df['LAYER_A_MEAN'] / setting.MAX + df['LAYER_B_MEAN']
-        for i in range(len(df)):
-            if consensus.iloc[i] > 0.95 and signed_CI.iloc[i] > 1.7:
-                pos_con += 1
-            elif consensus.iloc[i] > 0.95 and signed_CI.iloc[i] < -1.7:
-                neg_con += 1
-        return pos_con/len(df), neg_con/len(df)
 
-    def calculate_consensus_number_old(self, setting, df):  # 옛날 버전에 적용할 consensus 수 계산 인덱스
-        pos_con = 0
-        neg_con = 0
-        consensus = df['FRACTION_AB']
-        signed_CI = (df['LAYER_A_MEAN'] / setting.MAX) + df['LAYER_B_MEAN']
-        for i in range(len(df)):
-            if consensus.iloc[i] > 1.95 and signed_CI.iloc[i] > 1.9:
-                pos_con += 1
-            elif consensus.iloc[i] < 0.05 and signed_CI.iloc[i] < -1.9:
-                neg_con += 1
-        pos_con = pos_con / len(df)
-        neg_con = neg_con / len(df)
-        return pos_con, neg_con
-
-    def calculate_consensus_number_old2(self, setting, df):  # 옛날 버전에 적용할 consensus 수 계산 인덱스
+    def calculate_consensus_number(self, setting, df, a):  # 옛날 버전에 적용할 consensus 수 계산 인덱스
         pos_con = 0
         neg_con = 0
         signed_CI = (df['LAYER_A_MEAN'] / setting.MAX) + df['LAYER_B_MEAN']
         for i in range(len(df)):
-            if signed_CI.iloc[i] > 1.95:      # 원래 1.9
+            if signed_CI.iloc[i] > a:      # 원래 1.9
                 pos_con += 1
-            elif signed_CI.iloc[i] < -1.95:   # 원래 -1.9
+            elif signed_CI.iloc[i] < -a:   # 원래 -1.9
                 neg_con += 1
         pos_con = pos_con / len(df)
         neg_con = neg_con / len(df)
@@ -180,29 +122,22 @@ class AnalysisDB:
         plt.show()
         plt.close()
 
-    def making_mixed_hist_new(self, df):
-        fig = plt.figure()  # 그래프 창생성
-        ax = fig.add_subplot(111)
-        N = len(df)
-        tuples = self.making_tuple_data_new(df)
-        CIs = tuples[1]
-        PCRs = tuples[2]  # 남학생 수
-        NCRs = tuples[3]  # 여학생 수
-        ind = np.arange(N)  # x축
-        width = 0.2  # 너비
-        p1 = ax.bar(ind-(width/2), PCRs, width, color='SkyBlue')  # subplot에 bar chart 생성(남학생)
-        p2 = ax.bar(ind-(width/2), NCRs, width, color='IndianRed', bottom=PCRs)  # subplot에 bar chart 생성(여학생), bottom 옵션에 남학생 위에다 그리기
-        p3 = ax.bar(ind+(width/2), CIs, width, color='palegreen', label='AS')
-
-        ax.set_xlabel('model', fontsize=16)  # x축 라벨
-        ax.set_title('Competition results', fontsize=18)  # subplot의 제목
-        ax.set_yticks(np.arange(0, 1.2, 0.2))  # 0 ~ 81까지 10간격식으로 y축 틱설정
-        ax.set_xticks(ind)  # x축 틱설정
-        ax.set_xticklabels(tuples[0])  # x축 틱 라벨설정
-        ax.tick_params(labelsize=13)
-        plt.legend((p1[0], p2[0], p3[0]), ("PCR", "NCR", "AS total"), loc=0, fontsize=14)
-        plt.show()
-        plt.close()
+    def making_property_array_for_new(self, setting, step):
+        property_array = np.zeros(5)
+        df_original = self.select_db.select_data_from_DB(setting)
+        df_step = df_original[df_original.Steps == step]
+        model = df_step['MODEL']
+        model = sorted(model.unique())
+        for m in model:
+            df_model = df_step[df_step.MODEL == m]
+            total_AS = self.calculate_total_AS_new(df_model)
+            pcr, ncr = self.calculate_consensus_number_new(df_model)
+            initial_value = np.array([m, total_AS, pcr, ncr, pcr+ncr])
+            property_array = np.vstack([property_array, initial_value])
+        property_data = property_array[1:]
+        columns = ['Model', 'AS total', 'PCR', 'NCR', 'CR']
+        df = pd.DataFrame(property_data, columns=columns)
+        return df
 
     def making_mixed_hist(self, df):
         fig = plt.figure()  # 그래프 창생성
@@ -227,30 +162,6 @@ class AnalysisDB:
         plt.legend((p1[0], p2[0], p3[0]), ("PCR", "NCR", "AS total"), loc=0, fontsize=14)
         plt.show()
         plt.close()
-
-    def making_tuple_data_new(self, df):
-        model = []
-        As = []
-        pcrs = []
-        ncrs = []
-        cr = []
-        for i in df['Model']:
-            model.append(i)
-        model = tuple(model)
-        for i in df['AS total']:
-            As.append(eval(i))
-        As = tuple(As)
-        for i in df['PCR']:
-            pcrs.append(eval(i))
-        pcrs = tuple(pcrs)
-        for i in df['NCR']:
-            ncrs.append(eval(i))
-        ncrs = tuple(ncrs)
-
-        for i in df['CR']:
-            cr.append(eval(i))
-        cr = tuple(cr)
-        return model, As, pcrs, ncrs, cr
 
     def making_tuple_data(self, df):
         model = []
@@ -279,56 +190,112 @@ class AnalysisDB:
         cr = tuple(cr)
         return model, pcrs, ncrs, As, fr, cr
 
+    def calculate_total_AS_new(self, df):
+        total_AS = sum(df['AS']) / len(df)
+        return total_AS
+
+    def calculate_consensus_number_new(self, df):
+        pos_con = 0
+        neg_con = 0
+        signed_CI = df['AS']
+        for i in range(len(df)):
+            if signed_CI.iloc[i] > 0.95:
+                pos_con += 1
+            elif signed_CI.iloc[i] < -0.95:
+                neg_con += 1
+        return pos_con/len(df), neg_con/len(df)
+
+    def making_tuple_data_new(self, df):
+        model = []
+        As = []
+        pcrs = []
+        ncrs = []
+        cr = []
+        for i in df['Model']:
+            model.append(i)
+        model = tuple(model)
+        for i in df['AS total']:
+            As.append(eval(i))
+        As = tuple(As)
+        for i in df['PCR']:
+            pcrs.append(eval(i))
+        pcrs = tuple(pcrs)
+        for i in df['NCR']:
+            ncrs.append(eval(i))
+        ncrs = tuple(ncrs)
+
+        for i in df['CR']:
+            cr.append(eval(i))
+        cr = tuple(cr)
+        return model, As, pcrs, ncrs, cr
+
+    def making_mixed_hist_new(self, df):
+        fig = plt.figure()  # 그래프 창생성
+        ax = fig.add_subplot(111)
+        N = len(df)
+        tuples = self.making_tuple_data_new(df)
+        CIs = tuples[1]
+        PCRs = tuples[2]  # 남학생 수
+        NCRs = tuples[3]  # 여학생 수
+        ind = np.arange(N)  # x축
+        width = 0.2  # 너비
+        p1 = ax.bar(ind-(width/2), PCRs, width, color='SkyBlue')  # subplot에 bar chart 생성(남학생)
+        p2 = ax.bar(ind-(width/2), NCRs, width, color='IndianRed', bottom=PCRs)  # subplot에 bar chart 생성(여학생), bottom 옵션에 남학생 위에다 그리기
+        p3 = ax.bar(ind+(width/2), CIs, width, color='palegreen', label='AS')
+
+        ax.set_xlabel('model', fontsize=16)  # x축 라벨
+        ax.set_title('Competition results', fontsize=18)  # subplot의 제목
+        ax.set_yticks(np.arange(0, 1.2, 0.2))  # 0 ~ 81까지 10간격식으로 y축 틱설정
+        ax.set_xticks(ind)  # x축 틱설정
+        ax.set_xticklabels(tuples[0])  # x축 틱 라벨설정
+        ax.tick_params(labelsize=13)
+        plt.legend((p1[0], p2[0], p3[0]), ("PCR", "NCR", "AS total"), loc=0, fontsize=14)
+        plt.show()
+        plt.close()
  #'CI total', 'Fraction AB total', 'CR'
+
 if __name__ == "__main__":
     print("AnalysisDB")
     setting = Setting_Simulation_Value.Setting_Simulation_Value()
     analysis_db = AnalysisDB()
-    setting.database = 'paper_revised_data'
-    setting.table = 'simulation_table2'
-    df = analysis_db.making_property_array_for_new(setting, 100)
-    analysis_db.making_mixed_hist_new(df)
-
-    
+    # setting.database = 'paper_revised_data'
+    # setting.table = 'simulation_table2'
+    # df = analysis_db.making_property_array_for_new(setting, 100)
+    # analysis_db.making_mixed_hist_new(df)
+    # df = pd.read_pickle('df1')
+    # print(df.head())
 
 
     # setting.database = 'competition'
     # setting.table = 'result_db'
-    # df1 = analysis_db.making_property_array(setting, 30)
-    # df2 = analysis_db.making_property_array(setting, 100)
+    # df1 = analysis_db.making_property_array(setting, 30, 1.9)
+    # df2 = analysis_db.making_property_array(setting, 100, 1.95)
     # setting.database = 'paper_revised_data'
     # setting.table = 'simulation_table'
-    # df3 = analysis_db.making_property_array(setting, 100)
+    # df3 = analysis_db.making_property_array(setting, 100, 1.95)
     # df = pd.concat([df1, df2, df3], ignore_index=True)
     # df = df.iloc[:, [0, 6, 8, 9, 10]]
     # print(df)
     # pd.to_pickle(df, 'df1')
+    df = pd.read_pickle('df1')
+    # print(df)
+    # fig.6 # different structures
+    df = df.loc[[9, 8, 1, 0, 3], :]
+    df['Model'] = ['RR(5)-RR(5)', 'RR(5)-BA', 'BA-RR(5)', 'BA-BA', 'RR(10)-RR(5)']
+    analysis_db.making_mixed_hist_new(df)
 
-
-    # fig.6
-    # df = pd.concat([df1, df2], ignore_index=True)
-    # df = df.loc[[9, 8, 1, 0, 3], :]
-    # df['Model'] = ['RR(5)-RR(5)', 'RR(5)-BA', 'BA-RR(5)', 'BA-BA', 'RR(10)-RR(5)']
-    # analysis_db.making_mixed_hist(df)
-
-    # fig.5
-    # df = pd.concat([df1, df2], ignore_index=True)
+    # fig.5 # different internal edges
     # df = df.loc[[4, 5, 6, 9], :]
-    # analysis_db.making_mixed_hist(df)
-
-    # fig.4
-    # setting.database = 'paper_revised_data'
-    # setting.table = 'simulation_table'
-    # df3 = analysis_db.making_property_array(setting, 100)
-
-    # df = analysis_db.making_property_array_for_new(setting, 100)
     # analysis_db.making_mixed_hist_new(df)
 
-
-    # df = pd.concat([df2, df3], ignore_index=True)
+    # fig.4  # Hierarchical Model
     # df = df.loc[[0, 2, 4, 6, 1, 3, 5], :]
     # df['Model'] = ['RRM', 'HM(2)', 'HM(4)', 'HM(8)', 'HM(16)', 'HM(32)', 'HM(64)']
     # analysis_db.making_mixed_hist(df)
+
+
+    # df = pd.concat([df2, df3], ignore_index=True)
+
 
     # df = df.iloc[:, [0, 6, 8, 9, 10]]
     # print(df)
